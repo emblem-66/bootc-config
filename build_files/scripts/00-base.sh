debb4cf9674cba99b7e948b27db0480deb243cae
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-03-07 13:46:15 +0100

    Update 00-base.sh

--- build_files/scripts/00-base.sh
+++ build_files/scripts/00-base.sh
@@ -6,17 +6,16 @@ trap 'echo "::endgroup::"' EXIT
 
 dnf install -y 'dnf5-command(config-manager)'
 # glibc
-dnf install -y glibc-minimal-langpack
-dnf remove -y glibc-all-langpacks
+dnf install -y glibc-all-langpacks
 # Debloat
 dnf remove -y qemu-user-static* sssd*
 # Just
-dnf install -y just
+#dnf install -y just
 # Cockpit
 dnf install -y cockpit
-systemctl enable cockpit.socket
+#systemctl enable cockpit.socket
 # Firewall
 dnf install -y firewalld
-systemctl enable firewalld.service
+#systemctl enable firewalld.service
 # Autoremove
 dnf autoremove -y
