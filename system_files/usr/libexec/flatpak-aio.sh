0706598909838fc91b5213210d79a3ed55a3e222
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-03-07 14:11:37 +0100

    Update flatpak-aio.sh

--- system_files/usr/libexec/flatpak-aio.sh
+++ system_files/usr/libexec/flatpak-aio.sh
@@ -8,8 +8,13 @@ flatpak remote-modify --system --disable fedora || true
 
 # APP INSTALL
 FLATPAK_LIST="$(curl https://raw.githubusercontent.com/emblem-66/bootc-config/refs/heads/main/flatpak-packages | tr '\n' ' ')"
-flatpak --system -y --noninteractive install ${FLATPAK_LIST}
+flatpak -y --noninteractive install ${FLATPAK_LIST}
 
 # UPDATE
 flatpak update --noninteractive --assumeyes
 flatpak uninstall --unused --noninteractive --assumeyes
+
+# OVERRIDES
+flatpak override --filesystem=/var/mnt
+flatpak override --filesystem=xdg-config/gtk-3.0:ro
+flatpak override --filesystem=xdg-config/gtk-4.0:ro
