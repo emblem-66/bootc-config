8e5261ca4bfa39ad7cd9c32031fa1573e3ae39eb
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-03-17 09:35:04 +0100

    Update containers-aio.sh

--- system_files/common/usr/libexec/containers-aio.sh
+++ system_files/common/usr/libexec/containers-aio.sh
@@ -3,12 +3,27 @@ set -xeuo pipefail
 
 BASE_URL="https://raw.githubusercontent.com/emblem-66/containers/refs/heads/main/system_files"
 DIR="/etc/containers/systemd"
+
 services=""
+
+#tailscale_ip=""
+
+TS_IP=$(tailscale ip -4 | head -n1)
+
+if [[ -z "$TS_IP" ]]; then
+    echo "Error: could not get Tailscale IP"
+    exit 1
+fi
 
 for file in "$DIR"/*.container; do
     name=$(basename "$file")
 
     curl -fsSL "$BASE_URL/$name" -o "$DIR/$name"
+
+    # Only replace if placeholder exists
+    if grep -q "__TS_IP__" "$DIR/$name"; then
+        sed -i "s|__TS_IP__|$TS_IP|g" "$DIR/$name"
+    fi
 
     service="${name%.container}.service"
     services="$services $service"
